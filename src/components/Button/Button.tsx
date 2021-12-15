cdbfa694ae4fa7511589b36ddf9472f2d6177f8f
MuhammadHamza0546 <muhammad.hamza@ocloudsolutions.net>
2021-12-15 17:50:46 +0500

    first commit

--- src/components/Button/Button.tsx
+++
@@ -1,5 +0,0 @@
-import "./Button.css";
-const buttonComp = () => {
-  return <button className="Button">EXPORT</button>;
-};
-export default buttonComp;
--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,10 +1,17 @@
-import React, { ChangeEvent } from "react";
-export interface ICheckBox {
-  checked: boolean;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+import React from "react";
+interface IText{
+  text:string;
 }
-
-const Checkbox: React.FC<ICheckBox> = (props) => {
-  return <input type="checkbox" {...props} />;
+const Checkbox:React.FC<IText> = (props) => {
+  return (
+    <label className="myCheckbox  myCheckbox1">
+    <input type="checkbox" name="test" />
+    <span></span>
+    <label className="label-for">
+      {" "}
+     {props.text}
+    </label>
+  </label>
+  );
 };
 export default Checkbox;
